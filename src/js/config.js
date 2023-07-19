78af9f00ebbbfb064ba933077717e40a81249721
luttinno <luka.grdzelishvili1994@gmail.com>
2023-07-19 14:19:39 +0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 16;
+
+export const KEY = '00c9b42c-1fbe-4013-9607-7637ca351e83';
+
+export const MODAL_CLOSE_SEC = 2.5;
